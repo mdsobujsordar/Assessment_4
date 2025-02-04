e0c2a19151fce48ab7279bbc67d60ebec682738d
sobuj <sobuj.sordar@doer.com.bd>
2025-02-04 15:12:05 +0600

    minor change

--- cypress/e2e/02-filter-validation/filter-validation.po.js
+++ cypress/e2e/02-filter-validation/filter-validation.po.js
@@ -3,8 +3,8 @@ class filterValidation {
         cy.visit(data.URL);
     }
     filterItem(data) {
-        cy.get('[data-test="category-01JK6C2KR3ZA5B2VHJG0NX0W9W"]').scrollIntoView().should('be.visible').click();
-        cy.get('[data-test="brand-01JK6C2KPTA6HF69F7107HF52H"]').scrollIntoView().should('be.visible').click();
+        cy.contains('Hammer').scrollIntoView().should('be.visible').click();
+        cy.contains('MightyCraft Hardware').scrollIntoView().should('be.visible').click();
 
         //Assertion
         cy.get('[data-test="filter_completed"]').scrollIntoView().should('contain', data.item_name);
